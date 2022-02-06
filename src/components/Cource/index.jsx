273d6bb2de7935cc6c6364ee8c453264166a0d04
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-06 17:32:31 +0900

    ⚡️ 商品の並び順調整

--- src/components/Cource/index.jsx
+++ src/components/Cource/index.jsx
@@ -4,11 +4,17 @@ import { Card } from "src/components/Card";
 export function Cource() {
   return (
     <>
-      <section className={clsx("block my-0 mb-10 text-center w-full")}>
-        <h1 className={clsx("text-3xl bold mb-6")}>厳選和牛コースメニュー</h1>
+      <section
+        className={clsx(
+          "block my-0 lg:px-8 sm:px-6 px-4 mb-16 w-full text-center"
+        )}
+      >
+        <h1 className={clsx("text-3xl bold mb-6 text-start")}>
+          厳選和牛コースメニュー
+        </h1>
         <div
           className={clsx(
-            "flex items-center sm:justify-center flex-col lg:flex-row lg:gap-16"
+            "flex items-center sm:justify-center flex-col lg:flex-row lg:gap-x-6"
           )}
         >
           <Card
@@ -17,6 +23,7 @@ export function Cource() {
             content="タン / 焼野菜 / 特選ハラミ / キムチ"
             tab1="3500円"
             tab2="税込(3850円)"
+            alt="桜コース"
           />
           <Card
             title="椿コース"
@@ -24,6 +31,15 @@ export function Cource() {
             content="タン / 焼野菜 / 特選ハラミ / キムチ"
             tab1="5000円"
             tab2="税込(5500円)"
+            alt="椿コース"
+          />
+          <Card
+            title="飲み放題"
+            src="/Menu/beer.jpg"
+            content="タン / 焼野菜 / 特選ハラミ / キムチ"
+            tab1="1500円"
+            tab2="コース注文時のみ"
+            alt="飲み放題"
           />
         </div>
       </section>
