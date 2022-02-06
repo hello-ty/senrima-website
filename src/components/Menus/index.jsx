1e07924a6cba5cb311334822758a6f932c29f638
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-06 17:33:44 +0900

    ⚡️ 商品の並び順変更

--- src/components/Menus/index.jsx
+++ src/components/Menus/index.jsx
@@ -1,37 +1,39 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Menus() {
   const callouts = [
     {
-      name: "単品メニュー",
-      description: "おいしいメニューがたくさん",
+      name: "単品",
+      description: "厳選A5ランク和牛よりご用意してます。",
       imageSrc: "/Menu/tan.jpg",
       imageAlt: "単品メニュー",
-      href: "#",
+      href: "/products",
+      alt: "単品メニュー",
     },
     {
-      name: "ドリンク・飲み放題メニュー",
+      name: "ドリンクメニュー",
       description: "ワインからジュースまで揃ってます。",
       imageSrc: "/Menu/beer.jpg",
       imageAlt: "ドリンク",
       href: "#",
+      alt: "ドリンクメニュー",
     },
     {
-      name: "サブメニュー",
-      description: "おいしいアイスがたくさん",
-      imageSrc: "/Menu/ice.jpg",
+      name: "焼肉弁当",
+      description: "オーダー毎に丁寧に焼き上げます。",
+      imageSrc: "/Menu/meet.jpeg",
       imageAlt: "アイス",
       href: "#",
+      alt: "おいしい焼肉弁当",
     },
   ];
 
   return (
-    <div className="bg-gray-100 mb-10">
+    <div className="bg-white mb-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
-          <h2 className="text-2xl font-extrabold text-gray-900">
-            メニューリスト
-          </h2>
+          <h2 className="text-2xl font-extrabold text-gray-900">お品書き</h2>
 
           <div className="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
             {callouts.map((callout) => (
@@ -41,13 +43,16 @@ export default function Menus() {
                     src={callout.imageSrc}
                     layout="fill"
                     objectFit="cover"
+                    alt={callout.alt}
                   />
                 </div>
                 <h3 className="mt-6 text-sm text-gray-500">
-                  <a href={callout.href}>
-                    <span className="absolute inset-0" />
-                    {callout.name}
-                  </a>
+                  <Link href={callout.href}>
+                    <a>
+                      <span className="absolute inset-0" />
+                      {callout.name}
+                    </a>
+                  </Link>
                 </h3>
                 <p className="text-base font-semibold text-gray-900">
                   {callout.description}
