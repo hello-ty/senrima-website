18d3a67d6d52d0074bdb7c5e9e6a4c40400edea2
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-06 17:34:08 +0900

    ⚡️

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,17 +2,28 @@ import clsx from "clsx";
 import { Cource } from "src/components/Cource";
 import Menus from "src/components/Menus";
 import Access from "src/components/Access";
+import Image from "next/image";
 
 export function Main() {
   return (
     <>
       <main
         className={clsx(
-          "border  py-24 lg:px-24 md:px-20 sm:px-1 w-screen font-fancy"
+          "border  py-7 lg:px-24 md:px-10 sm:px-1 w-screen font-fancy"
         )}
       >
-        <Cource />
+        <div className="w-full h-64 mt-24">
+          <figure className="relative w-96 h-full block mx-auto">
+            <Image
+              src="/Header/logo.png"
+              layout="fill"
+              objectFit="cover"
+              alt="ロゴ"
+            />
+          </figure>
+        </div>
         <Menus />
+        <Cource />
         <Access />
       </main>
     </>
