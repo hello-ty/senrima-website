abd9a0c8e29fbfc48cfe38cf884a4fa2f7b9e06e
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-06 17:31:29 +0900

    ✨ ロゴ中央に持ってきた

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -5,21 +5,35 @@ import clsx from "clsx";
 export function Banner() {
   return (
     <>
-      <div className={clsx("border w-screen h-[calc(100vh-4rem)]")}>
+      <div className={clsx("border w-screen h-[calc(100vh-4rem)] font-fancy")}>
         <div className={clsx("relative w-full h-full bg-slate-800")}>
           <figure className={classes.image}>
             <Image
               src="/Banner/senrimadoor.jpg"
               layout="fill"
               objectFit="cover"
+              alt="千里馬入り口"
             />
           </figure>
           <figure className={classes.image}>
-            <Image src="/Banner/second.jpg" layout="fill" objectFit="cover" />
+            <Image
+              src="/Banner/second.jpg"
+              layout="fill"
+              objectFit="cover"
+              alt="千里馬二階テーブル"
+            />
           </figure>
           <figure className={classes.image}>
-            <Image src="/Banner/interior.jpg" layout="fill" objectFit="cover" />
+            <Image
+              src="/Banner/interior.jpg"
+              layout="fill"
+              objectFit="cover"
+              alt="千里馬内装"
+            />
           </figure>
+          <div className={clsx(classes.img)}>
+            <h1 className={clsx("text-white text-5xl")}>厳選和牛</h1>
+          </div>
         </div>
       </div>
     </>
