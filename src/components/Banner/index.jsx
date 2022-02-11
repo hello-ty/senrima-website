b7e728293a9b7a724ba153b21296447399630a87
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-11 11:25:23 +0900

    🎨 トップをビデオから画像切り替えに変更

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,20 +3,29 @@ import classes from "/src/components/Banner/Banner.module.css";
 import clsx from "clsx";
 
 export function Banner() {
+  const ImageSrc = [
+    { src: "/Banner/interior.jpg", alt: "千里馬内装" },
+    { src: "/Banner/bakedmeel.png", alt: "千里馬内装" },
+    { src: "/Banner/senrimadooor.jpg", alt: "千里馬内装" },
+  ];
+
   return (
     <>
-      <div
-        className={clsx("overflow-hidden border w-screen h-auto font-fancy")}
-      >
-        <div className={clsx(" h-full  bg-slate-800", classes.banner)}>
-          <video
-            autoPlay
-            loop
-            muted
-            width={"100%"}
-            src="/topmovie.mp4"
-            className=""
-          ></video>
+      <div className={clsx("border w-screen h-[calc(100vh-4rem)] font-fancy")}>
+        <div className={clsx("relative h-full w-full bg-slate-800")}>
+          {ImageSrc.map((item, index) => (
+            <figure
+              key={index}
+              className={clsx("absolute h-full w-full opacity-0", classes.img)}
+            >
+              <Image
+                src={item.src}
+                layout="fill"
+                objectFit="cover"
+                alt={item.alt}
+              />
+            </figure>
+          ))}
           <figure
             className={clsx(
               "w-60 h-32 md:w-56 md:h-40 lg:w-96 lg:h-64 z-30",
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -18,7 +18,7 @@ function classNames(...classes) {
 
 export default function Header() {
   return (
-    <Disclosure as="nav" className={clsx("bg-white z-50 relative")}>
+    <Disclosure as="nav" className={clsx("bg-white")}>
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 font-fancy">
