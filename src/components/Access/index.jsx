7325c11c4b2412b0451429c0b1674d60e73f7212
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-12 10:14:53 +0900

    📝 予約時間修正

--- src/components/Access/index.jsx
+++ src/components/Access/index.jsx
@@ -35,7 +35,7 @@ export default function Access() {
           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">予約可否</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              16時~予約可
+              17時~予約可
             </dd>
           </div>
           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
