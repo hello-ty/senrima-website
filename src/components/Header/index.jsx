3e93067c554b271eb4f1cc34041732bc5add8a80
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-12 10:16:14 +0900

    📝 tel番号変更

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -89,7 +89,7 @@ export default function Header() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="tel:000-0000-0000"
+                            href="tel:052-446-7257"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
