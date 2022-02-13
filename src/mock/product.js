710f2595a4e08ffb131f349642b6b984d74eb4f3
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-13 14:50:11 +0900

    🎨 定数をまとめた

---
+++ src/mock/product.js
@@ -0,0 +1,114 @@
+export const products = {
+  meet: [
+    {
+      description: "何書こう",
+      name: "ヒレステーキ",
+      imageSrc: "/Product/hire.jpg",
+      imageAlt: "ヒレステーキ",
+    },
+    {
+      description: "豚ののどぶえ、コリコリした食感が特徴です。",
+      name: "塩ナンコツ",
+      imageSrc: "/Product/nankotu.jpg",
+      imageAlt: "塩ナンコツ",
+    },
+    {
+      description: "大腸の中でも特にやわらかく希少性の高い上質部位",
+      name: "上ホルモン",
+      imageSrc: "/Product/tettyan.jpg",
+      imageAlt: "上ホルモン",
+    },
+    {
+      description: "何書こう",
+      name: "焼きしゃぶ",
+      imageSrc: "/Product/yakisyabu.jpg",
+      imageAlt: "焼きしゃぶ",
+    },
+    {
+      description: "脂質が少なく、あっさりした肉本来の旨味が特徴",
+      name: "ロース",
+      imageSrc: "/NewMenu/ro-su.jpeg",
+      imageAlt: "ロース",
+    },
+    {
+      description: "脂の甘さも肉の旨味もしっかりと濃く肉質は高い",
+      name: "上カルビ",
+      imageSrc: "/NewMenu/zyoukarubi.jpeg",
+      imageAlt: "上カルビ",
+    },
+    {
+      description: "何書こう ",
+      name: "ミックスホルモン",
+      imageSrc: "/NewMenu/mikkusuhorumon.jpeg",
+      imageAlt: "ミックスホルモン",
+    },
+    {
+      description: "歯切れはさっくりと優しく豊かな旨味が口中に広がります",
+      name: "塩タン",
+      imageSrc: "/NewMenu/tan.jpeg",
+      imageAlt: "塩タン",
+    },
+  ],
+  salad: [
+    {
+      description: `赤：コチュジャンベースのピリ辛サラダ\n白：韓国のり・胡椒油を使い、風味豊かに仕上げました`,
+      name: "韓国サラダ",
+      imageSrc: "/Product/kannkoku.jpg",
+      imageAlt: "韓国サラダ",
+    },
+    {
+      description: "Machined Mechanical Pencil",
+      name: "特選上レバー",
+      imageSrc: "/Product/reba.jpg",
+      imageAlt:
+        "Hand holding black machined steel mechanical pencil with brass tip and top.",
+    },
+    {
+      description: "Machined Mechanical Pencil",
+      name: "せんまい",
+      imageSrc: "/Product/senmai.jpg",
+      imageAlt:
+        "Hand holding black machined steel mechanical pencil with brass tip and top.",
+    },
+  ],
+  tukemono: [
+    {
+      description: "白菜",
+      name: "白菜キムチ",
+      imageSrc: "/Product/kimuti.jpg",
+      imageAlt: "白菜キムチ",
+    },
+    {
+      description: "大根",
+      name: "カクテキ",
+      imageSrc: "/NewMenu/kakuteki.jpg",
+      imageAlt: "カクテキ",
+    },
+    {
+      description: "きゅうり",
+      name: "オイキムチ",
+      imageSrc: "/NewMenu/oikimuti.jpeg",
+      imageAlt: "オイキムチ",
+    },
+    {
+      description: "",
+      name: "キムチ盛合せ",
+      imageSrc: "/NewMenu/kimutimoriawase.jpeg",
+      imageAlt: "キムチ盛合せ",
+    },
+  ],
+  tinmi: [
+    {
+      description: "もやし・ほうれん草・ぜんまい・大根の和え物",
+      name: "ナムル　777円",
+      imageSrc: "/Product/namuru.jpg",
+      imageAlt: "ナムル",
+    },
+    {
+      description: "大豆もやしを韓国風に仕上げました",
+      name: "ピリ辛もやし　777円",
+      imageSrc: "/NewMenu/pirikaramoyasi.jpeg",
+      imageAlt: "ピリ辛もやし",
+    },
+  ],
+};
---
+++ src/mock/tablemenu.js
@@ -0,0 +1,53 @@
+export const tableMenu = {
+  meet: [
+    {
+      name: "ネギ塩たん",
+      description: "味付けネギとタンとの相性が抜群です",
+      price: 777,
+    },
+    {
+      name: "塩ミノ",
+      description: "上ミノの特に上質な部位だけを薄切りにした当店自慢の一品です",
+      price: 777,
+    },
+    {
+      name: "三河地鶏",
+      description: "朝挽き三河地鶏にゆず胡椒を添えて",
+      price: 777,
+    },
+    {
+      name: "塩ハツ",
+      description: "心臓、ビタミン、タンパク質・鉄分・タウリンを多く含みます",
+      price: 777,
+    },
+    {
+      name: "サムギョプサル",
+      description:
+        "黒豚の三段バラ（付け合わせのチシャ葉・大葉・ネギ・ニンニクスライスを包んでお召し上がりください）",
+      price: 777,
+    },
+  ],
+  tinmi: [
+    {
+      name: "チャンジャ",
+      description: "鱈の内臓を使った韓国塩辛",
+      price: 777,
+    },
+    {
+      name: "韓国のり",
+      description: "胡麻油風味、ご飯のお供に是非どうぞ",
+      price: 777,
+    },
+    {
+      name: "胡麻の葉",
+      description: "胡麻の葉の醤油付け",
+      price: 777,
+    },
+    {
+      name: "青唐辛子",
+      description:
+        "特製辛みそを付けてお召し上がりください。きざみスライスもご用意できます",
+      price: 777,
+    },
+  ],
+};
