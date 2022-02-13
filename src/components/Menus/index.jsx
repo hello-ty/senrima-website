13ba04277a4d2a8d64e07e99df19a594097dd1d6
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-13 14:51:11 +0900

    🎨 定数を別ファイルに移動。コードを見やすくする

--- src/components/Menus/index.jsx
+++ src/components/Menus/index.jsx
@@ -1,34 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
+import { menulists } from "/src/mock/menu.js";
 
 export default function Menus() {
-  const callouts = [
-    {
-      name: "単品",
-      description: "厳選A5ランク和牛よりご用意してます。",
-      imageSrc: "/Menu/tan.jpg",
-      imageAlt: "単品メニュー",
-      href: "/products",
-      alt: "単品メニュー",
-    },
-    {
-      name: "ドリンクメニュー",
-      description: "ワインからジュースまで揃ってます。",
-      imageSrc: "/Menu/beer.jpg",
-      imageAlt: "ドリンク",
-      href: "/PDF/twodrink.pdf",
-      alt: "ドリンクメニュー",
-    },
-    {
-      name: "焼肉弁当",
-      description: "オーダー毎に丁寧に焼き上げます。",
-      imageSrc: "/Menu/toptanbentou.jpeg",
-      imageAlt: "アイス",
-      href: "#",
-      alt: "おいしい焼肉弁当",
-    },
-  ];
-
   return (
     <div className="bg-white mb-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -36,7 +10,7 @@ export default function Menus() {
           <h2 className="text-2xl font-extrabold text-gray-900">お品書き</h2>
 
           <div className="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
-            {callouts.map((callout) => (
+            {menulists.map((callout) => (
               <div key={callout.name} className="group relative">
                 <div className="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
                   <Image
--- src/components/ProductLists/index.jsx
+++ src/components/ProductLists/index.jsx
@@ -1,87 +1,6 @@
 import Image from "next/image";
-
-const products = [
-  {
-    description: "",
-    name: "厚切りタン",
-    imageSrc: "/Product/hire.jpg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-  },
-  {
-    description: "Machined Mechanical Pencil",
-    name: "塩ミノ",
-    imageSrc: "/Product/nankotu.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  {
-    description: "Machined Mechanical Pencil",
-    name: "特選ハラミ",
-    imageSrc: "/Product/tettyan.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  {
-    description: "Machined Mechanical Pencil",
-    name: "上カルビ",
-    imageSrc: "/Product/yakisyabu.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  {
-    description: "Nomad Tumbler",
-    name: "ロース",
-    imageSrc: "/NewMenu/ro-su.jpeg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
-  },
-  {
-    description: "Nomad Tumbler",
-    name: "上カルビ",
-    imageSrc: "/NewMenu/zyoukarubi.jpeg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
-  },
-];
-
-const productsSalad = [
-  {
-    description: "Focus Paper Refill",
-    name: "韓国サラダ",
-    imageSrc: "/Product/kannkoku.jpg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
-  },
-  {
-    description: "Machined Mechanical Pencil",
-    name: "白菜キムチ",
-    imageSrc: "/Product/kimuti.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  {
-    description: "Machined Mechanical Pencil",
-    name: "ナムル",
-    imageSrc: "/Product/namuru.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  {
-    description: "Machined Mechanical Pencil",
-    name: "特選上レバー",
-    imageSrc: "/Product/reba.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  {
-    description: "Machined Mechanical Pencil",
-    name: "せんまい",
-    imageSrc: "/Product/senmai.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-];
+import { MenuTable } from "src/components/MenuTable";
+import { products } from "/src/mock/product.js";
 
 export const ProductLists = () => {
   return (
@@ -90,7 +9,7 @@ export const ProductLists = () => {
         <h2 className="text-center text-3xl mb-10">肉（塩・タレ）</h2>
 
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
+          {products.meet.map((product) => (
             <p key={product.name} className="group">
               <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                 <figure className="relative w-full h-52 object-center object-cover group-hover:opacity-75">
@@ -112,11 +31,14 @@ export const ProductLists = () => {
           ))}
         </div>
       </div>
+
+      <MenuTable title="meet" />
+
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-center text-3xl mb-10">サラダ</h2>
 
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {productsSalad.map((product) => (
+          {products.salad.map((product) => (
             <p key={product.name} className="group">
               <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                 <figure className="relative w-full h-52 object-center object-cover group-hover:opacity-75">
@@ -138,6 +60,61 @@ export const ProductLists = () => {
           ))}
         </div>
       </div>
+
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-center text-3xl mb-10">漬物</h2>
+
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.tukemono.map((product) => (
+            <p key={product.name} className="group">
+              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                <figure className="relative w-full h-52 object-center object-cover group-hover:opacity-75">
+                  <Image
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </figure>
+              </div>
+              <h3 className="mt-4 text-sm text-gray-700">
+                {product.description}
+              </h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">
+                {product.name}
+              </p>
+            </p>
+          ))}
+        </div>
+      </div>
+
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-center text-3xl mb-10">珍味</h2>
+
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.tinmi.map((product) => (
+            <p key={product.name} className="group">
+              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                <figure className="relative w-full h-52 object-center object-cover group-hover:opacity-75">
+                  <Image
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </figure>
+              </div>
+              <h3 className="mt-4 text-sm text-gray-700">
+                {product.description}
+              </h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">
+                {product.name}
+              </p>
+            </p>
+          ))}
+        </div>
+      </div>
+      <MenuTable title="tinmi" />
     </div>
   );
 };
