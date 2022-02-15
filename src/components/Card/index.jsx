fdc0f745b96743ee8c4acc78fe81faadc88c9f7e
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-15 23:32:16 +0900

    🏗️ カードの幅を固定

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 export function Card(props) {
   return (
     <>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg mb-10 lg:mb-0">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg mb-10 lg:mb-0 h-96">
         <figure className={clsx("relative w-full h-52 border bg-slate-500")}>
           <Image
             src={props.src}
@@ -13,11 +13,11 @@ export function Card(props) {
             alt={props.alt}
           />
         </figure>
-        <div className="px-6 py-4">
+        <div className="px-6 py-3">
           <div className="font-bold text-xl mb-2">{props.title}</div>
-          <p className="text-gray-700 text-base">{props.content}</p>
+          <p className="text-gray-700 text-base text-left">{props.content}</p>
         </div>
-        <div className="px-6 pt-4 pb-2">
+        <div className="px-6">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             {props.tab1}
           </span>
