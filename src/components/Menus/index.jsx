a2e5182d20b8b5a09e0a4e72b83dca5d5ad05294
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-23 16:37:19 +0900

    💄  UI修正

--- src/components/Menus/index.jsx
+++ src/components/Menus/index.jsx
@@ -5,11 +5,13 @@ import { menulists } from "/src/mock/menu.js";
 export default function Menus() {
   return (
     <div className="bg-white mb-10">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
-          <h2 className="text-2xl font-extrabold text-gray-900">お品書き</h2>
+          <h2 className="text-3xl text-center font-extrabold text-gray-900">
+            お品書き
+          </h2>
 
-          <div className="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
+          <div className="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-20">
             {menulists.map((callout) => (
               <div key={callout.name} className="group relative">
                 <div className="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
