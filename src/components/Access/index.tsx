bb2afcb8971223c1d2783260a0a55b38aef31e67
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-03-12 16:22:12 +0900

    📝  文章の配置変え

--- src/components/Access/index.tsx
+++ src/components/Access/index.tsx
@@ -7,10 +7,10 @@ export const Access: VFC = () => {
     <div className="bg-white shadow overflow-hidden sm:rounded-lg">
       <div className="px-4 py-5 sm:px-6">
         <h3 className="text-lg leading-6 font-medium text-gray-900">
-          会社概要
+          店舗概要
         </h3>
         <p className="mt-1 max-w-2xl text-sm text-gray-500">
-          Company Information
+          Store Information
         </p>
       </div>
       <div className="border-t border-gray-200">
@@ -22,24 +22,6 @@ export const Access: VFC = () => {
             </dd>
           </div>
           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">電話番号</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              052-446-7257
-            </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">営業時間</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              17:00~23:00(年中無休)
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">予約可否</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              17時~予約可
-            </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">
               1階
               <br />
@@ -56,7 +38,7 @@ export const Access: VFC = () => {
               </div>
             </dd>
           </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">2階</dt>
             <dd className="mt-1 text-sm lg:flex text-gray-900 sm:mt-0 sm:col-span-2">
               <div className="relative w-full lg:w-1/2 mb-5 lg:mr-5 h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
@@ -79,7 +61,35 @@ export const Access: VFC = () => {
             <dt></dt>
             <dd>40名様まで宴会できます</dd>
           </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">席数</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              ２階：テーブル　７席(4〜8人)
+              <br />
+              １階：テーブル　１席(6人)　/　カウンター　5席(1席/2人)
+              <br />
+              ※団体様(10名様以上の予約可能)
+            </dd>
+          </div>
           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">営業時間</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              17:00~23:00(年中無休)
+            </dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">予約可否</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              17時~予約可
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">電話番号</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              052-446-7257
+            </dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">住所</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
               〒453-0015 愛知県名古屋市中村区椿町１８−１６
@@ -91,16 +101,6 @@ export const Access: VFC = () => {
                   loading="lazy"
                 ></iframe>
               </figure>
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">席数</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              ２階：テーブル　７席(4〜8人)
-              <br />
-              １階：テーブル　１席(6人)　/　カウンター　5席(1席/2人)
-              <br />
-              ※団体様(10名様以上の予約可能)
             </dd>
           </div>
         </dl>
