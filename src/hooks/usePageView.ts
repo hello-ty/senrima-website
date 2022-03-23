ab27744aee04827c9667c55c1111238d6afba810
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-03-23 18:02:12 +0900

    ✨  GA編集

--- src/hooks/usePageView.ts
+++ src/hooks/usePageView.ts
@@ -1,23 +1,22 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-import * as gtag from "../lib/gtag";
+import { existsGaId, pageview } from "../lib/gtag";
 
 export default function usePageView() {
   const router = useRouter();
-  console.log(gtag.GA_ID);
+
   useEffect(() => {
-    if (!gtag.existsGaId) {
+    if (!existsGaId) {
       return;
     }
 
-    const handleRouteChange = (path: any, { shallow }: any) => {
-      if (!shallow) {
-        gtag.pageview(path);
-      }
+    const handleRouteChange = (path: any) => {
+      pageview(path);
     };
 
     router.events.on("routeChangeComplete", handleRouteChange);
+
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
