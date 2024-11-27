30f7e0af2655db99365587e596ed02a82b883e5a
Tatsuki Yasui <yasui@nxr-inc.com>
2024-11-28 00:31:24 +0900

    チラシ表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Access } from "../components/Access";
 import { Banner } from "../components/Banner";
 import { Cource } from "../components/Cource";
 import { Menus } from "../components/Menus";
+import { Flyer } from "../components/Flyer";
 import { Layout } from "../layout/";
 
 const Home: VFC = () => {
@@ -17,6 +18,7 @@ const Home: VFC = () => {
         <>
           <Banner />
           <main className="border  py-7 lg:px-24 md:px-10 sm:px-1 w-screen font-fancy">
+            <Flyer />
             <Menus />
             <Cource />
             <Access />
